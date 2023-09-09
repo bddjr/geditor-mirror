de6590ec948db8a6e3c20fe81824591a5c73c195
bddjr <ygyh5019@outlook.com>
2023-09-09 09:47:32 +0800

    造型与声音 换源

--- har-to-files.js
+++ har-to-files.js
@@ -1,68 +1,81 @@
 // node har-to-files.js
 
+// 是否开启不压缩模式
+const dev_mode = false;
+
 //(async()=>{
+
+console.log(`dev mode ${dev_mode}`);
 
 const fs = require('fs');
 const path = require('path');
 const UglifyJS = require("uglify-js");
 
 const input_file_name = path.join(__dirname,
+    // 输入的har文件名
     'geditor.shiki.online.har'
 );
 const ouput_file_pathname = path.join(__dirname,
+    // 输出文件夹名称
     'ouput'
 );
 
-//try{ fs.rmdirSync(ouput_file_pathname); }catch{}
-try{ fs.mkdirSync(ouput_file_pathname); }catch{}
-
 const har_data = JSON.parse( fs.readFileSync(input_file_name).toString() ).log;
 const har_page_url = har_data.pages[0].title;
-const har_entries = har_data.entries;
 
 const read_finished_pathnames = [];
 
-for(let i of har_entries){
+for(const i of har_data.entries){
     if(
         !i.request.url.startsWith(har_page_url)
         ||
         i._error
-        //||
-        //i.response.status == 304
     ) continue;
+
     const url = new URL(i.request.url);
     if (read_finished_pathnames.includes(url.pathname)) continue;
+
     const response_content = i.response.content;
+
+    let writetext = response_content.text;
+    if (writetext === undefined) continue;
+
     let writepath = path.join(ouput_file_pathname, url.pathname);
-    if (url.pathname == '/') writepath = path.join(writepath, 'index.html');
+    if (url.pathname == '/') writepath = path.join(writepath, 'index.html'); //首页自动补全文件名
     console.log(writepath);
+
     if (url.pathname.lastIndexOf('/') != 0){
-        let dirnames = url.pathname.split('/').slice(1,-1);
+        // 检测含有多个斜杠，自动补全创建文件夹
+        const dirnames = url.pathname.split('/').slice(1,-1);
         let dir = ouput_file_pathname;
         for(let i of dirnames){
             dir = path.join(dir, i);
             try{ fs.mkdirSync(dir) }catch{}
         }
     }
-    let writetext = response_content.text;
-    if (writetext === undefined) continue;
     if (url.pathname.endsWith('.js')){
+        // 优化JS文件
+        if (response_content.encoding == 'base64'){
+            writetext = Buffer.from(writetext, 'base64').toString('utf-8');
+        }
+        if (url.pathname == '/lib.min.js'){
+            // 造型与声音 换源
+            writetext = writetext.replace(
+                '"https://cdn.assets.scratch.mit.edu/internalapi/asset/".concat(iconMd5, "/get/")',
+                '`https://m.ccw.site/user_projects_assets/${iconMd5}`'
+            ).replace(
+                '"".concat(this.assetHost, "/internalapi/asset/").concat(asset.assetId, ".").concat(asset.dataFormat, "/get/")',
+                '`https://m.ccw.site/user_projects_assets/${asset.assetId}.${asset.dataFormat}`'
+            )
+        }
         fs.writeFileSync(
             writepath,
-            UglifyJS.minify(
-                response_content.encoding == 'base64'
-                ? Buffer.from(writetext, 'base64').toString('utf-8')
-                : writetext
-            ).code
-        );
-    }else if (response_content.encoding == 'base64'){
-        writetext = Buffer.from(writetext, 'base64');
-        fs.writeFileSync(
-            writepath,
-            (Buffer.from(writetext, 'base64')),
-            { encoding: null, flag: 'w' }
+            dev_mode ? writetext : UglifyJS.minify( writetext ).code
         );
     }else{
+        if (response_content.encoding == 'base64'){
+            writetext = Buffer.from(writetext, 'base64');
+        }
         fs.writeFileSync(writepath, writetext);
     }
     read_finished_pathnames.push(url.pathname);
